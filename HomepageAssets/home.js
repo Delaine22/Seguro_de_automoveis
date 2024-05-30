6a3336ef5f7c4b008df669f1dd37508d28c8f7d0
Delaine22 <108828746+Delaine22@users.noreply.github.com>
2024-05-30 17:20:03 +0100

    update home.js

--- HomepageAssets/home.js
+++ HomepageAssets/home.js
@@ -6,3 +6,21 @@ accordions.forEach(accordion => {
         body.classList.toggle('active');
     })
 })
+
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('#introducao',{
+    duration:2000,
+    rotate:{y:18}
+});
+sr.reveal('#feedback',{
+    duration:2000,
+    rotate:{y:18}
+});
+sr.reveal('#accordion',{
+    duration:2000,
+    rotate:{y:18}
+});
+sr.reveal('#footer',{
+    duration:2000,
+    rotate:{y:18}
+});
